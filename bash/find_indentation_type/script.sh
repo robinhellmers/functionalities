f20baeec944529ce627783e4167f5819bcbf0f4a
Robin Hellmers <robin@hellmers.se>
2023-05-25 11:55:22 +0200

    Bash: Move test files into separate directory
    - Why:
    To keep the repository cleaner and easier to find the
    functionalities of each directory.
    
    - New/Changes:
    Created new directory test_files and moved the example files there.
    
    Reference those files correctly in functionality script.
    
    - Testing:
    No difference.

--- bash/find_indentation_type/script.sh
+++ bash/find_indentation_type/script.sh
@@ -2,10 +2,12 @@
 
 main()
 {
+    test_files_location="test_files"
+
     test_files=()
-    test_files+=("example_1")
-    test_files+=("example_2")
-    test_files+=("example_3")
+    test_files+=("$test_files_location/example_1")
+    test_files+=("$test_files_location/example_2")
+    test_files+=("$test_files_location/example_3")
 
     for file in "${test_files[@]}"
     do
